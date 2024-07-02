4d33b22e92890241fa8fe8aa17ed254d9c17dea7
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-07-02 12:26:43 -0300

    Atualizando .gitignore para excluir arquivos desnecessários

---
+++ TaskManagement.Domain/Entities/TaskList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Entities
+{
+    public class TaskList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    }
+}
