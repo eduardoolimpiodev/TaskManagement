4445eb043b2137c3b72ba369327f07b684e1c257
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-07-02 20:22:33 -0300

    fix: Atualização da classe de Contexto para incluir o DbSet<User>.

--- TaskManagement.Infrastructure/Data/TaskManagementContext.cs
+++ TaskManagement.Infrastructure/Data/TaskManagementContext.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaskManagement.Domain.Entities;
 
 namespace TaskManagement.Infrastructure.Data
@@ -17,6 +12,7 @@ namespace TaskManagement.Infrastructure.Data
 
         public DbSet<TaskList> TaskLists { get; set; }
         public DbSet<TaskItem> TaskItems { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
