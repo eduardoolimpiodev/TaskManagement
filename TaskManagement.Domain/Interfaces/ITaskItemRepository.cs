a881b7c775d01610f2373e6b81fc7fa8ef890cfa
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-07-02 12:29:12 -0300

    feat: Criada as interfaces.

---
+++ TaskManagement.Domain/Interfaces/ITaskItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Domain.Interfaces
+{
+    public interface ITaskItemRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllByTaskListIdAsync(int taskListId);
+        Task<TaskItem> GetByIdAsync(int id);
+        Task AddAsync(TaskItem taskItem);
+        Task UpdateAsync(TaskItem taskItem);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ TaskManagement.Domain/Interfaces/ITaskListRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Domain.Interfaces
+{
+    public interface ITaskListRepository
+    {
+        Task<IEnumerable<TaskList>> GetAllAsync();
+        Task<TaskList> GetByIdAsync(int id);
+        Task AddAsync(TaskList taskList);
+        Task UpdateAsync(TaskList taskList);
+        Task DeleteAsync(int id);
+    }
+}
