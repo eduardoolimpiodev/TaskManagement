b5e127b169b29caa0b7f6e5bfb640a7946e4040f
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-07-02 20:48:11 -0300

    fix: Atualização do TaskItemsController no projeto *.WebApp para incluir o JWT nos cabeçalhos das requisições Http.

--- TaskManagement.WebApp/Controllers/TaskItemsController.cs
+++ TaskManagement.WebApp/Controllers/TaskItemsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Text;
+using Microsoft.AspNetCore.Http;
 
 namespace TaskManagement.WebApp.Controllers
 {
@@ -17,24 +18,36 @@ namespace TaskManagement.WebApp.Controllers
             _httpClient = httpClient;
         }
 
+        private void SetAuthorizationHeader()
+        {
+            var token = HttpContext.Session.GetString("JWTToken");
+            if (!string.IsNullOrEmpty(token))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+
         public async Task<IActionResult> Index(int taskListId)
         {
+            SetAuthorizationHeader();
             var response = await _httpClient.GetAsync($"https://localhost:5001/api/taskitems?taskListId={taskListId}");
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStringAsync();
-            var taskItems = JsonConvert.DeserializeObject<IEnumerable<TaskManagement.Domain.Entities.TaskItem>>(content);
+            var taskItems = JsonConvert.DeserializeObject<IEnumerable<TaskItem>>(content);
+            ViewBag.TaskListId = taskListId;
             return View(taskItems);
         }
 
         public IActionResult Create(int taskListId)
         {
-            var taskItem = new TaskManagement.Domain.Entities.TaskItem { TaskListId = taskListId };
+            var taskItem = new TaskItem { TaskListId = taskListId };
             return View(taskItem);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(TaskManagement.Domain.Entities.TaskItem taskItem)
+        public async Task<IActionResult> Create(TaskItem taskItem)
         {
+            SetAuthorizationHeader();
             var content = new StringContent(JsonConvert.SerializeObject(taskItem), Encoding.UTF8, "application/json");
             var response = await _httpClient.PostAsync("https://localhost:5001/api/taskitems", content);
             response.EnsureSuccessStatusCode();
@@ -43,16 +56,18 @@ namespace TaskManagement.WebApp.Controllers
 
         public async Task<IActionResult> Edit(int id)
         {
+            SetAuthorizationHeader();
             var response = await _httpClient.GetAsync($"https://localhost:5001/api/taskitems/{id}");
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStringAsync();
-            var taskItem = JsonConvert.DeserializeObject<TaskManagement.Domain.Entities.TaskItem>(content);
+            var taskItem = JsonConvert.DeserializeObject<TaskItem>(content);
             return View(taskItem);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(TaskManagement.Domain.Entities.TaskItem taskItem)
+        public async Task<IActionResult> Edit(TaskItem taskItem)
         {
+            SetAuthorizationHeader();
             var content = new StringContent(JsonConvert.SerializeObject(taskItem), Encoding.UTF8, "application/json");
             var response = await _httpClient.PutAsync($"https://localhost:5001/api/taskitems/{taskItem.Id}", content);
             response.EnsureSuccessStatusCode();
@@ -61,6 +76,7 @@ namespace TaskManagement.WebApp.Controllers
 
         public async Task<IActionResult> Delete(int id, int taskListId)
         {
+            SetAuthorizationHeader();
             var response = await _httpClient.DeleteAsync($"https://localhost:5001/api/taskitems/{id}");
             response.EnsureSuccessStatusCode();
             return RedirectToAction(nameof(Index), new { taskListId });
